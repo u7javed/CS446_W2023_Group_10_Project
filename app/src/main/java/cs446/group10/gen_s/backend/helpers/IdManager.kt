2f0ac11fe3aec7698593c59ab75c628b0bc6e33e
Umer Javed <uj2553@gmail.com>
2023-03-03 17:53:01 -0500

    Created ViewModel file with template class declaration

---
+++ app/src/main/java/cs446/group10/gen_s/backend/helpers/IdManager.kt
@@ -0,0 +1,14 @@
+object IdManager {
+    /***
+     * IdManager is a Singleton class that manages and generates all IDs for the app
+     ***/
+
+    val globalIDTable: MutableMap<String, Any> = mutableMapOf()
+
+    // TODO: Create a robust system for generating IDs
+    fun generateId() {
+        
+    }
+
+
+}
---
+++ app/src/main/java/cs446/group10/gen_s/backend/view_model/ViewModel.kt
@@ -0,0 +1,7 @@
+class ViewModel() {
+
+    init {
+
+    }
+
+}
