0946830f4527920a99429dbe5159e27c9158ebba
Umer Javed <uj2553@gmail.com>
2023-03-04 15:29:53 -0500

    Create ViewModelHelper for tasks such as name and date input sanitization

--- app/src/main/java/cs446/group10/gen_s/backend/view_model/ViewModel.kt
+++ app/src/main/java/cs446/group10/gen_s/backend/view_model/ViewModel.kt
@@ -1,7 +1,20 @@
-class ViewModel() {
+import java.time.LocalDate
+
+class ViewModel {
 
     init {
 
+    }
+
+    // TODO: Update events and preferences to their corresponding list types
+    fun createPlan(name: String, startDate: LocalDate, endDate: LocalDate, events: List<Any>, preferences: List<Any>) {
+        val sanitizedName: String = ViewModelHelper.sanitizePlanName(name)
+
+    }
+
+    private fun isValidDate(startDate: LocalDate, endDate: LocalDate): Boolean {
+        // TODO: Validate a given start and end date for an event
+        return true
     }
 
 }
